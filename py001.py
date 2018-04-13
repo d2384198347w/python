1aadc87d1749fd0d62d95b35603b2f991527a4ee
root <wangnan.alvin@gmail.com>
2018-04-13 11:24:37 +1000

    1

---
+++ py001.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+x = 9
+y = 2
+print(x**y) # 81
+print(x/y) # 4.5
+print(x//y) # 4
+
+print(x and y) # 2
+print(x or y) # 9
+print(not x) # False
+
+z = [1, 2, 3]
+print(x in z) # False
+print(x not in z) # True
+print(y in z) # True
